4816b6911e06cdd5b780070c10e52dda72475aa1
KrSiBu <ksh010113@gmail.com>
2024-05-21 16:35:00 +0900

    고상혁

--- app/src/main/java/com/example/mypagetalk/SplashActivity.kt
+++ app/src/main/java/com/example/mypagetalk/SplashActivity.kt
@@ -3,17 +3,44 @@ package com.example.mypagetalk
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mypagetalk.auth.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
+        auth = Firebase.auth
+
+        if(auth.currentUser?.uid == null) {
+            Log.d("SplashActivity", "null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }, 3000)
+
+        } else {
+            Log.d("SplashActivity", "not null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -21,10 +48,11 @@ class SplashActivity : AppCompatActivity() {
         }
 
         // splash에서 login으로 화면 전환(3초 딜레이)
-        Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }, 3000)
+        // 상단 splash에서 login main 으로 보내는거 완료
+        //Handler().postDelayed({
+        //    startActivity(Intent(this, LoginActivity::class.java))
+        //    finish()
+        //}, 3000)
 
     }
 }
